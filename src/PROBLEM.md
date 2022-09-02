add668c2c501864b7a4455a1ff322adeb6380e2d
HP <mdzaid1299@gmail.com>
2022-09-02 22:05:40 +0530

    initial commit

---
+++ src/PROBLEM.md
@@ -0,0 +1,18 @@
+Array questions:
+.........................................
+Baby solve these by yourself don't Google..
+@1. Print sum of the array elements. Take size of the array from user. And take  elements of the array from user .
+
+@2. Print maximum and minimum elements of an array.Take size of the array from user. 
+And take elements of the array from user .
+Ex: 12, 34,56,98,6
+O/p: max= 98
+Min= 6
+
+@3. Take size of the array from user. And take elements of the array from user .
+And print the array elements in reverse order.
+Ex: 10 ,23 ,45
+O/p: 45, 23, 10
+*These 3 questions you have to solve tomorrow and then share it with me ..
+*Notes: Don't copy it it from other sites.. try it by yourself.. if there is any problem ask me.. i will explain you. But I am not gonna reveal you the solution..
+#Happy code#
--- src/Sum.java
+++ src/Sum.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Sum {
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter size of array");
+        int size = scanner.nextInt();
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+            sum = sum + array[i];
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+        System.out.println(sum);
+        int max = array[0];
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
+            }
+            if (min > array[i]) {
+                min = array[i];
+            }
+        }
+        System.out.println("max = " + max);
+        System.out.println("min = " + min);
+    }
 }
